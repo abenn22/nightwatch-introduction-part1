30b2ab715d765bcb3b9cb2bf3a4c565f4d7b1b1d
April <aapilcher@hotmail.com>
2018-01-24 09:07:19 -0700

    3.2 stretch

---
+++ nightwatch/step4/supporting/data.js
@@ -0,0 +1,11 @@
+module.exports = {
+    simpleAddition: {
+        buttons: ['2','+','2','='],
+        result: '4'
+    },
+    decimalMultiplication: {
+        buttons: ['3','2','.','1','*','2','='],
+        result: '64.2'
+    },
+
+}
--- nightwatch/step4/tests/tests.js
+++ nightwatch/step4/tests/tests.js
@@ -1,5 +1,6 @@
 const selectors = require('../supporting/selectors')
 const functions = require('../supporting/functions')
+const data = require('../supporting/data')
 
 module.exports = {
     beforeEach : browser => {
@@ -12,26 +13,22 @@ module.exports = {
     '2+2=4' : browser => {
         //I click all the appropriate buttons and check the display for the appropriate results, per the steps of my test case
         browser
-            .click(selectors['2'])
-            .expect.element(selectors['result']).text.to.equal('2')
-        browser
-            .click(selectors['+'])
-            .expect.element(selectors['result']).text.to.equal('0')
-        browser
-            .click(selectors['2'])
-            .expect.element(selectors['result']).text.to.equal('2')
-        browser
-            .click(selectors['='])
-            .expect.element(selectors['result']).text.to.equal('4')
+            functions.buttonClicker(browser, data.simpleAddition.buttons[0])
+            functions.buttonClicker(browser, data.simpleAddition.buttons[1])
+            functions.buttonClicker(browser, data.simpleAddition.buttons[2])
+            functions.buttonClicker(browser, data.simpleAddition.buttons[3])
+            browser.expect.element(selectors['result']).text.to.equal(data.simpleAddition.result)
     },
     '32.1*2=64.2' : browser => {
-        functions.buttonClicker(browser, '3')
-        functions.buttonClicker(browser, '2')
-        functions.buttonClicker(browser, '.')
-        functions.buttonClicker(browser, '1')
-        functions.buttonClicker(browser, '*')
-        functions.buttonClicker(browser, '2')
-        functions.buttonClicker(browser, '=')
-        browser.expect.element(selectors['result']).text.to.equal('64.2')
+        browser
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[0])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[1])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[2])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[3])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[4])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[5])
+            functions.buttonClicker(browser, data.decimalMultiplication.buttons[6])
+            browser.expect.element(selectors['result']).text.to.equal(data.decimalMultiplication.result)
     }
+
 }
